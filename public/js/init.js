6d2d6414f49dacdb245695cade3199f91bc0b9da
Adamo Figueroa <adamo.figueroa@gnp.com.mx>
2020-08-05 03:06:24 -0500

    Agregando idiomas

--- public/js/init.js
+++ public/js/init.js
@@ -5,6 +5,17 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+
+  var instances = M.Dropdown.init(elems, {});
+});
+
+// Or with jQuery
+
+$('.dropdown-trigger').dropdown();
+
+
 $('.modal').modal();
 var seccionActual = $("#yo-img");
 
